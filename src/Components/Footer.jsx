38d39e8a6c9e733646e1f813f74f11cbf3523dba
NivethaRajkumar <nivethas1207@gmail.com>
2024-07-10 17:01:40 +0530

    Completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className=" footer bg-dark">
+      <footer className="py-3 ">
+        <p className="text-center footer-text pt-2">© @Nivetha</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
